2c39135236d77b2ff89389455bddfadd0c6e7721
anuj <anuj6202188@gmail.com>
2026-09-23 17:07:23 +0530

    added vercel frontend url in cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -66,6 +66,7 @@ app.add_middleware(
         "http://localhost:5174",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
+        "agri-queue-blond.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
